11c7d7181eb281164caa7b61cc793a2507d6df6e
myaotti1979-hub <myaotti1979@gmail.com>
2026-05-31 21:00:08 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'stgym-v69';
+const CACHE_NAME = 'stgym-v70';
 self.addEventListener('install', e => { self.skipWaiting(); });
 self.addEventListener('activate', e => {
   e.waitUntil(caches.keys().then(keys =>
